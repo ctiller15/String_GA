79172a610e322f5e964470b347925bdc230ffaf8
Tiller <ctiller@proforma.com>
2018-05-08 11:12:44 -0400

    Create function to randomly generate string of characters

--- String_GA/Individual.cs
+++ String_GA/Individual.cs
@@ -10,7 +10,7 @@ namespace String_GA
     {
         public int Length { get; set; }
         public int Fitness { get; set; }
-        public List<string> Genes { get; set; }
+        public List<string> Genes { get; set; } = new List<string>();
 
         public void GetLength(int maxLength)
         {
@@ -18,9 +18,32 @@ namespace String_GA
             Console.WriteLine(Length);
         }
 
+        public char GetRandomCharacter()
+        {
+            // 32 = min, 32 + 94 = max.
+            int minVal = 32;
+            int maxVal = 126;
+            int num = Services.Rand.Next(minVal, maxVal + 1);
+            char letter = Convert.ToChar(num);
+            return letter;
+        }
+
+        public void PopulateGenes()
+        {
+            for(int i = 0; i < Length; i++)
+            {
+                var strChar = GetRandomCharacter();
+
+                Genes.Add(Convert.ToString(GetRandomCharacter()));
+            }
+            Console.WriteLine(string.Join("", Genes));
+        }
+
         public Individual()
         {
             GetLength(Population.Matchedstring.Count() * 2);
+            PopulateGenes();
+
         }
     }
 }
